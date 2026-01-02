73f4b70c727f80b720b6d8b75dba42cfe8f96e11
Rajeel Siddiqui github <159783237+RajeelSiddiqui1@users.noreply.github.com>
2026-01-02 20:32:14 +0500

    minor bug fixed

--- app/api/employee/tasks/[id]/route.js
+++ app/api/employee/tasks/[id]/route.js
@@ -172,7 +172,7 @@ export async function PUT(req, { params }) {
           type: "employee_status_update",
           title: "Employee Status Updated",
           message: `${employee.firstName} ${employee.lastName} updated task "${task.formId?.title}" status to ${status}`,
-          link: `/manager/submissions/${task._id}`,
+          link: `/manager/submissions/detail/${task._id}`,
           referenceId: task._id,
           referenceModel: "FormSubmission",
         })
